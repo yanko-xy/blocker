b286d27f48dc5a19e376f346c9c76227117f2113
赵晨炀 <904852749@qq.com>
2024-08-17 16:57:08 +0800

    feat: Transaction mempool

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"blocker/crypto"
 	"blocker/node"
 	"blocker/proto"
+	"blocker/util"
 	"context"
 	"log"
 	"time"
@@ -12,18 +14,28 @@ import (
 
 func main() {
 
-	makeNode(":3000", []string{})
+	makeNode(":3000", []string{}, true)
 	time.Sleep(time.Second)
-	makeNode(":4000", []string{":3000"})
+	makeNode(":4000", []string{":3000"}, false)
 
-	time.Sleep(time.Second * 4)
-	makeNode(":5000", []string{":4000"})
+	time.Sleep(time.Second)
+	makeNode(":5000", []string{":4000"}, false)
 
-	select {}
+	for {
+		time.Sleep(time.Second * 2)
+		makeTransaction()
+	}
 }
 
-func makeNode(listenAddr string, bootstrapNodes []string) *node.Node {
-	n := node.NewNode()
+func makeNode(listenAddr string, bootstrapNodes []string, isValidator bool) *node.Node {
+	cfg := node.ServerConfig{
+		Version:    "Blocker-1",
+		ListenAddr: listenAddr,
+	}
+	if isValidator {
+		cfg.PrivateKey = crypto.GeneratePrivateKey()
+	}
+	n := node.NewNode(cfg)
 	go n.Start(listenAddr, bootstrapNodes)
 	return n
 }
@@ -35,14 +47,25 @@ func makeTransaction() {
 	}
 
 	c := proto.NewNodeClient(client)
-
-	version := &proto.Version{
-		Version:    "blocker-0.1",
-		Height:     1,
-		ListenAddr: ":4000",
+	privKey := crypto.GeneratePrivateKey()
+	tx := &proto.Transaction{
+		Version: 1,
+		Inputs: []*proto.TxInput{
+			{
+				PrevTxHash:   util.RandomHash(),
+				PrevOutIndex: 0,
+				PublicKey:    privKey.Public().Bytes(),
+			},
+		},
+		Outputs: []*proto.TxOutput{
+			{
+				Amount:  99,
+				Address: privKey.Public().Address().Bytes(),
+			},
+		},
 	}
 
-	_, err = c.Handshake(context.TODO(), version)
+	_, err = c.HandleTransaction(context.TODO(), tx)
 	if err != nil {
 		log.Fatal(err)
 	}
