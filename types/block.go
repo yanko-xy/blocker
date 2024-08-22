8356bfadabecb87dc83dcb21ada6388c9fbf47d9
赵晨炀 <904852749@qq.com>
2024-08-22 19:30:51 +0800

    feat:  Merkle Trees

--- types/block.go
+++ types/block.go
@@ -3,15 +3,41 @@ package types
 import (
 	"blocker/crypto"
 	"blocker/proto"
+	"bytes"
 	"crypto/sha256"
 
+	"github.com/cbergoon/merkletree"
 	pb "google.golang.org/protobuf/proto"
 )
 
+type TxHash struct {
+	hash []byte
+}
+
+func NewTxHash(hash []byte) TxHash {
+	return TxHash{hash: hash}
+}
+
+func (h TxHash) CalculateHash() ([]byte, error) {
+	return h.hash, nil
+}
+
+func (h TxHash) Equals(other merkletree.Content) (bool, error) {
+	equals := bytes.Equal(h.hash, other.(TxHash).hash)
+	return equals, nil
+}
+
 func VerifyBlock(b *proto.Block) bool {
+	if len(b.Transactions) == 0 {
+		if !VerifyRootHash(b) {
+			return false
+		}
+	}
+
 	if len(b.PublicKey) != crypto.PubKeyLen {
 		return false
 	}
+
 	if len(b.Signature) != crypto.SignatureLen {
 		return false
 	}
@@ -27,7 +53,49 @@ func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
 	sig := pk.Sign(hash)
 	b.PublicKey = pk.Public().Bytes()
 	b.Signature = sig.Bytes()
+
+	if len(b.Transactions) > 0 {
+		tree, err := GetMerkleTree(b)
+		if err != nil {
+			panic(err)
+		}
+		b.Header.RootHash = tree.MerkleRoot()
+	}
 	return sig
+}
+
+func VerifyRootHash(b *proto.Block) bool {
+	tree, err := GetMerkleTree(b)
+	if err != nil {
+		return false
+	}
+
+	valid, err := tree.VerifyTree()
+	if err != nil {
+		return false
+	}
+
+	if !valid {
+		return false
+	}
+
+	return bytes.Equal(b.Header.RootHash, tree.MerkleRoot())
+}
+
+func GetMerkleTree(b *proto.Block) (*merkletree.MerkleTree, error) {
+	list := make([]merkletree.Content, len(b.Transactions))
+	for i := 0; i < len(b.Transactions); i++ {
+		list[i] = NewTxHash(HashTransaction(b.Transactions[i]))
+	}
+
+	//Create a new Merkle Tree from the list of Content
+	t, err := merkletree.NewTree(list)
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
+
 }
 
 // HashBlock returns a SHA256 of the header
--- types/block_test.go
+++ types/block_test.go
@@ -2,11 +2,26 @@ package types
 
 import (
 	"blocker/crypto"
+	"blocker/proto"
 	"blocker/util"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCalculateRootHash(t *testing.T) {
+	var (
+		privKey = crypto.GeneratePrivateKey()
+		block   = util.RandomBlock()
+		tx      = &proto.Transaction{
+			Version: 1,
+		}
+	)
+	block.Transactions = append(block.Transactions, tx)
+	SignBlock(privKey, block)
+	assert.True(t, VerifyRootHash(block))
+	assert.Equal(t, 32, len(block.Header.RootHash))
+}
 
 func TestSignVerifyBlock(t *testing.T) {
 	var (
